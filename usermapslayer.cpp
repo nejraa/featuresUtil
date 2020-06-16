d17fc3141e0caf75f99b35d70bd4bd7c61534227
Haris Dajic <hdajic1@etf.unsa.ba>
2020-06-16 08:36:48 +0200

    Added comments to UserMasLayer

--- usermapslayer.cpp
+++ usermapslayer.cpp
@@ -12,8 +12,8 @@ CUserMapsLayer::CUserMapsLayer(QQuickItem *parent)
 	: CBaseLayer(parent)
 {
 	setAcceptedMouseButtons(Qt::AllButtons);
-	QObject::connect(CUserMapsManager::instance(), &CUserMapsManager::selectedObjTypeChanged, this, &CUserMapsLayer::selectedObjType);
-	m_selectedObjPoints = QVector<QPointF>();
+	QObject::connect(CUserMapsManager::instance(), &CUserMapsManager::selectedObjTypeChanged,
+					 this, &CUserMapsLayer::selectedObjType);
 }
 
 CUserMapsLayer::~CUserMapsLayer()
@@ -30,7 +30,6 @@ CUserMapsLayer::~CUserMapsLayer()
 ////////////////////////////////////////////////////////////////////////////////
 void CUserMapsLayer::mousePressEvent(QMouseEvent *event)
 {
-	qDebug() << "Entered in mousePressEvent";
 	m_onPressTimer.start(2000);
 	m_startPoint = event->screenPos();
 }
@@ -49,7 +48,6 @@ void CUserMapsLayer::mouseMoveEvent(QMouseEvent *event)
 		m_onPressTimer.stop();
 		m_isMoving = true;
 
-		//Check if this is correct
 		QPointF pointDifference = m_startPoint - event->screenPos();
 		for (int i = 0; i < m_selectedObjPoints.size(); i++)
 			m_selectedObjPoints[i] -= pointDifference;
@@ -66,8 +64,7 @@ void CUserMapsLayer::mouseMoveEvent(QMouseEvent *event)
 void CUserMapsLayer::mouseReleaseEvent(QMouseEvent *event)
 {
 	setObjectPosition();
-	m_isMoving = false;
-	qDebug() << "Entered in mouseReleaseEvent";
+	m_isMoving = false; //Do i need this?
 	Q_UNUSED(event);
 }
 
@@ -83,6 +80,13 @@ QQuickFramebufferObject::Renderer *CUserMapsLayer::createRenderer() const
 	return new CUserMapsRenderer();
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn         CUserMapsLayer::selectedObjType(EUserMapObjectType objType)
+///
+/// \brief		Based on object type it gets the position points.
+///
+/// \param		objType Type of object.
+////////////////////////////////////////////////////////////////////////////////
 void CUserMapsLayer::selectedObjType(EUserMapObjectType objType)
 {
 	m_objectType = objType;
@@ -99,6 +103,13 @@ void CUserMapsLayer::selectedObjType(EUserMapObjectType objType)
 	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn		CUserMapsLayer::convertGeoVectorToPixelVector(QVector<CPosition> objPoints)
+///
+/// \brief	Converts vector of Geo coordinates into vector of pixel coordinates.
+///
+/// \param	objPoints Vectro of points.
+////////////////////////////////////////////////////////////////////////////////
 void CUserMapsLayer::convertGeoVectorToPixelVector(QVector<CPosition> objPoints)
 {
 	CCoordinates c;
@@ -108,6 +119,13 @@ void CUserMapsLayer::convertGeoVectorToPixelVector(QVector<CPosition> objPoints)
 	}
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn         CUserMapsLayer::convertGeoPointToPixelVector(CPosition objPoint)
+///
+/// \brief      Converts Geo point into pixel vector.
+///
+/// \param		objPoint Point of the selected object.
+////////////////////////////////////////////////////////////////////////////////
 void CUserMapsLayer::convertGeoPointToPixelVector(CPosition objPoint)
 {
 	CCoordinates c;
@@ -115,6 +133,13 @@ void CUserMapsLayer::convertGeoPointToPixelVector(CPosition objPoint)
 	m_selectedObjPoints.append(c.convertGeoCoordsToPixelCoords(objPoint));
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn         CUserMapsLayer::convertPixelVectorToGeoVector()
+///
+/// \brief      Converts vectro of pixel coordinates into vector of geo coordinates.
+///
+/// \return		Vector with geo coordinates.
+////////////////////////////////////////////////////////////////////////////////
 QVector<CPosition> CUserMapsLayer::convertPixelVectorToGeoVector()
 {
 	CCoordinates c;
@@ -125,6 +150,11 @@ QVector<CPosition> CUserMapsLayer::convertPixelVectorToGeoVector()
 	return geoPoints;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+/// \fn         CUserMapsLayer::setObjectPosition()
+///
+/// \brief      Sets object position based on his type.
+////////////////////////////////////////////////////////////////////////////////
 void CUserMapsLayer::setObjectPosition()
 {
 	QVector<CPosition> objPosition = convertPixelVectorToGeoVector();
